43e315c889786c984133d43d28da6226e0946b1b
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-03-21 01:59:38 -0400

    Add PrivateInternetAccess VPN config

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,9 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+
+      # VPN
+      ./pia.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
---
+++ pia.nix
@@ -0,0 +1,30 @@
+# Configuration for Private Internet Access VPN
+{ ... }:
+
+{ services.openvpn = {
+  servers.pia = {
+    autoStart = false;
+    config = ''
+      client
+      dev tun
+      proto udp
+      remote us-newyorkcity.privateinternetaccess.com 1198
+      resolv-retry infinite
+      nobind
+      persist-key
+      persist-tun
+      cipher aes-128-cbc
+      auth sha1
+      tls-client
+      remote-cert-tls server
+      auth-user-pass /home/bhipple/.pia/auth-user-pass
+      comp-lzo
+      verb 1
+      reneg-sec 0
+      crl-verify /home/bhipple/.pia/crl.rsa.2048.pem
+      ca /home/bhipple/.pia/ca.rsa.2048.crt
+      disable-occ
+    '';
+    };
+  };
+}
