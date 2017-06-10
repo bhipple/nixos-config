5d445784f1308a43e3ee04ee372513e5cf792d52
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-06-10 11:11:05 -0400

    Added storage drive

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/sdb";
   boot.loader.grub.efiSupport = false;
 
   networking.hostName = "brh.desktop";
