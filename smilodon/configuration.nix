1b9dc99b468b4aa43ca2133a338632b16b41d6cf
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-07-18 23:10:43 -0400

    Add chipple

--- smilodon/configuration.nix
+++ smilodon/configuration.nix
@@ -27,6 +27,15 @@
   users.extraUsers.smilodon = {
     isNormalUser = true;
     description = "Smilodon";
-    extraGroups = [ "adbusers" "smilodon" "docker" "ipfs" "networkmanager" "wheel" ];
+    extraGroups = [ "smilodon" "users" "ipfs" "networkmanager" ];
+  };
+
+  users.extraUsers.chipple = {
+    isNormalUser = true;
+    description = "Chris Hipple";
+    extraGroups = [ "chipple" "users" "smilodon" "ipfs" "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "AAAAB3NzaC1yc2EAAAADAQABAAABAQC1MQEzpAYvopxVvjsK/3ycEXpSmtqE5oa1Px+TcR5XoZ2H++q4X9CoA0yAa/H3eCVTDd3Kz62iibDHe7+T6YvAgpk6r5tiUGkoFu0TKnukwgsBI5nGgMzUKFo/yWRuNrqDAm+OGzjBhOhD3754i4TQijF23BScHX/DgjLCIn3iil0DsoAj5h0tLpPUSlWXBoJsWmh7r6UN9w8oOvUZQAYpsS0Ch1EFMxItmqnZO5PVpH/giwXxrq2Smx4GSCxV/RdsORGbjWv97vxsf7SoXAivtRYBIg2e4GK0zpPpQOdJFXc9xVsEMpXhk+XsaXfG0Jqo4tdxMhBJA6Sjq02iAc2F"
+    ];
   };
 }
