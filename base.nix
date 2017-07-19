cc0bc4ee27aa0ad34ba760ecd141c09f77e90f99
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-07-18 23:22:36 -0400

    Add bhipple to users group

--- base.nix
+++ base.nix
@@ -104,7 +104,7 @@
   users.extraUsers.bhipple = {
     isNormalUser = true;
     description = "Benjamin Hipple";
-    extraGroups = [ "adbusers" "bhipple" "docker" "ipfs" "networkmanager" "wheel" ];
+    extraGroups = [ "adbusers" "bhipple" "docker" "ipfs" "networkmanager" "users" "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuP/Z+l3KI5g0SmZ4s0vv3N2OdUEAwc9sBuFG1d8XYLbJEaaTONBGh2sSzMjqNnetyl1e9lz/Yn/406gksjhweR6A1AqTd/Ty8CKzzOD7YCu0QBMOo+hgHxO3LQS+WbB3ygAjZ/uzakSDwaAxHQcZgRiRNFIf+zadQgJ+4sVB2NDTGL16L1Ok252NpBfGdnojU51E2ZnQLj6Dq5ZgrpLrfZEAJkPqcmcjl2IhoFDveSxTvvmoxBAIbZYgHdQY/hSz7k3UOBIBNNV2u6XxW1pr35ySwJVGFav+6KiZR1IaHAka9GW8EeX0Au9QEmUVIZcdWrJHOB4xO4sgSCKaiI7bB"
     ];
