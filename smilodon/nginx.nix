cb2cef1266b99dee2cc80fd7040e450af599c8e9
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-07-22 23:51:43 -0400

    Cleanup php code

--- smilodon/nginx.nix
+++ smilodon/nginx.nix
@@ -19,14 +19,6 @@
             index = "index.html";
             extraConfig = "autoindex on;";
           };
-
-          # "~ \.php$" = {
-          #   index = "index.php";
-          #   extraConfig = ''
-          #     include ${pkgs.nginx}/conf/fastcgi_params;
-          #     fastcgi_pass unix:/run/phpfpm/smilodonpool.sock;
-          #   '';
-          # };
         };
 
         # TODO
@@ -36,30 +28,4 @@
 
     };
   };
-
-  # services.phpfpm = {
-  #   poolConfigs = {
-  #     smilodonpool = ''
-  #       user = phpfpm
-  #       group = phpfpm
-  #       listen = /run/phpfpm/smilodonpool.sock
-  #       listen.owner = nginx
-  #       listen.group = nginx
-  #       pm = dynamic
-  #       pm.max_children = 75
-  #       pm.start_servers = 10
-  #       pm.min_spare_servers = 5
-  #       pm.max_spare_servers = 20
-  #       pm.max_requests = 500
-  #     '';
-  #   };
-  # };
-
-  # users.extraUsers.phpfpm = {
-  #   description = "PHP FastCGI user";
-  #   uid = 2222;
-  #   group = "phpfpm";
-  # };
-
-  # users.extraGroups.phpfpm.gid = 2222;
 }
