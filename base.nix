d25f4e6f7b36207a14c75b087498f35eef7b2b85
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-07-23 14:31:27 -0400

    Install mailutils at system level

--- base.nix
+++ base.nix
@@ -16,8 +16,8 @@
   # System profile packages
   environment.systemPackages = with pkgs; [
     dmenu
-    gitAndTools.hub
     gitAndTools.gitFull
+    mailutils
     wget
     xlibs.xmessage
 
@@ -109,6 +109,10 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuP/Z+l3KI5g0SmZ4s0vv3N2OdUEAwc9sBuFG1d8XYLbJEaaTONBGh2sSzMjqNnetyl1e9lz/Yn/406gksjhweR6A1AqTd/Ty8CKzzOD7YCu0QBMOo+hgHxO3LQS+WbB3ygAjZ/uzakSDwaAxHQcZgRiRNFIf+zadQgJ+4sVB2NDTGL16L1Ok252NpBfGdnojU51E2ZnQLj6Dq5ZgrpLrfZEAJkPqcmcjl2IhoFDveSxTvvmoxBAIbZYgHdQY/hSz7k3UOBIBNNV2u6XxW1pr35ySwJVGFav+6KiZR1IaHAka9GW8EeX0Au9QEmUVIZcdWrJHOB4xO4sgSCKaiI7bB"
     ];
+  };
+
+  users.extraGroups.mail = {
+    name = "mail";
   };
 
   # The NixOS release to be compatible with for stateful data such as databases.
