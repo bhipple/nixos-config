53643e369eff8e1e43de485d70c8ab0e0fea5544
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-08-13 22:00:15 -0400

    Firewall on brh-base

--- brh-base.nix
+++ brh-base.nix
@@ -3,4 +3,10 @@
 {
   services.xserver.windowManager.default = "xmonad";
   services.xserver.desktopManager.default = "none";
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 8080 ];
+    allowedUDPPorts = [ 22 34197 ];
+  };
 }
