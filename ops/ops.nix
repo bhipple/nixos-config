f34588379e189423d98a692cd7527ac5d25be1e4
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-10-08 14:27:25 -0400

    Working EBS volume with auto-partition and mount

--- ops/ops.nix
+++ ops/ops.nix
@@ -1,5 +1,6 @@
 let
   region = "us-east-1";
+  zone = "us-east-1a";
   accessKeyId = "bhipple";
 in
 {
@@ -10,12 +11,23 @@ in
       deployment.targetEnv = "ec2";
       deployment.ec2.accessKeyId = accessKeyId;
       deployment.ec2.region = region;
+      deployment.ec2.zone = zone;
       deployment.ec2.instanceType = "t2.micro";
       deployment.ec2.keyPair = resources.ec2KeyPairs.my-key-pair;
 
       # WIP
       deployment.ec2.blockDeviceMapping = {
-        "/dev/xvdg".disk = resources.ebsVolumes.lokidata;
+        "/dev/xvdg".disk = resources.ebsVolumes.bigdata;
+      };
+
+      fileSystems = {
+      "/mnt/storage" = {
+        device = "/dev/xvdg";
+        label = "ebs-volume";
+        fsType = "ext4";
+        autoFormat = true;
+      };
+
       };
 
       imports = [
@@ -26,11 +38,10 @@ in
   # Provision an EC2 key pair.
   resources.ec2KeyPairs.my-key-pair = { inherit region accessKeyId; };
 
-  resources.ebsVolumes.lokidata = {
-    inherit region accessKeyId;
-    name = "Loki EBS";
+  resources.ebsVolumes.bigdata = {
+    inherit region accessKeyId zone;
+    name = "ebs-volume";
     size = 100;
     volumeType = "standard";
-    zone = "us-east-1a";
   };
 }
