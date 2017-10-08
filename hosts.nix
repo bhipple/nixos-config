bbb6ba90e9b68183156ea7a056aea7d905cb4887
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-10-07 21:47:06 -0400

    More concise expression

--- hosts.nix
+++ hosts.nix
@@ -3,12 +3,10 @@
 
 let
   commit = "76dee94b624dd53013a50684aba9ee61b031bfd7";
-
-  hostsfile = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/StevenBlack/hosts/${commit}/hosts";
-    sha256 = "163lms67j0k9yqp1xhdfq04w168z4j422zj7f7wxl5h2q28srfx5";
-  };
 in
 {
-  networking.extraHosts = builtins.readFile hostsfile;
+  networking.extraHosts = builtins.readFile (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/StevenBlack/hosts/${commit}/hosts";
+    sha256 = "163lms67j0k9yqp1xhdfq04w168z4j422zj7f7wxl5h2q28srfx5";
+  });
 }
