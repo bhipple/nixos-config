0ed61390f72c043e060cc440b886a46448cf8d32
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-10-08 13:34:31 -0400

    Working on EBS volume

--- ops/ops.nix
+++ ops/ops.nix
@@ -13,12 +13,24 @@ in
       deployment.ec2.instanceType = "t2.micro";
       deployment.ec2.keyPair = resources.ec2KeyPairs.my-key-pair;
 
+      # WIP
+      deployment.ec2.blockDeviceMapping = {
+        "/dev/xvdg".disk = resources.ebsVolumes.lokidata;
+      };
+
       imports = [
         ../base.nix
       ];
     };
 
   # Provision an EC2 key pair.
-  resources.ec2KeyPairs.my-key-pair =
-    { inherit region accessKeyId; };
+  resources.ec2KeyPairs.my-key-pair = { inherit region accessKeyId; };
+
+  resources.ebsVolumes.lokidata = {
+    inherit region accessKeyId;
+    name = "Loki EBS";
+    size = 100;
+    volumeType = "standard";
+    zone = "us-east-1a";
+  };
 }
