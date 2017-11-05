407abc74a03f80053bd141584f0ce2fae9428d55
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-11-05 10:35:23 -0500

    Working on config

--- brh.desktop/configuration.nix
+++ brh.desktop/configuration.nix
@@ -27,38 +27,45 @@
 
   services = {
     xserver = {
+      enable = true;
+
       # See config in /etc/X11/xorg.conf
       exportConfiguration = true;
 
-      #resolutions = [{ x = 2560; y = 1600; } { x = 2560; y = 1600; } { x = 2560; y = 1600; }];
-
       # Use proprietary AMD drivers
-      videoDrivers = [ "amdgpu-pro" "ati" ];
-      #videoDrivers = [ "ati" ];
+      #videoDrivers = [ "amdgpu-pro" "ati" ];
+      videoDrivers = [ "amdgpu-pro" ];
+
+      # serverLayoutSection = ''
+      #   Option "Xinerama" "on"
+      # '';
 
       xrandrHeads = [
-        # {
-        #   # Left Monitor
-        #   output = "DVI-D-2";
-        #   monitorConfig = ''
-        #     Option "Rotate" "left"
-        #   '';
-        # }
+        {
+          # Left Monitor
+          output = "DVI-D-0";
+          monitorConfig = ''
+            Option "Rotate" "left"
+            #Option "Position" "0" "0"
+          '';
+        }
         {
           # Center Monitor
           output = "DisplayPort-0";
           primary = true;
           monitorConfig = ''
             Option "Rotate" "left"
+            #Option "Position" "1600" "0"
           '';
         }
-        # {
-        #   # Right Monitor
-        #   output = "DVI-D-1";
-        #   monitorConfig = ''
-        #     Option "Rotate" "left"
-        #   '';
-        # }
+        {
+          # Right Monitor
+          output = "DVI-D-1";
+          monitorConfig = ''
+            Option "Rotate" "left"
+            #Option "Position" "3200" "0"
+          '';
+        }
       ];
     };
   };
