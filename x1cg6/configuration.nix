cfbc1b7ce0bb358f0cd2bc92fd8a23040af5697e
System administrator <root@nixos.home>
2018-12-25 01:10:12 +0000

    Adding X1 Carbon Gen6; remove Smilodon config

---
+++ x1cg6/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      ../base.nix
+      ../brh-base.nix
+      # ../hosts.nix
+      ../xserver.nix
+
+      # VPN
+      # ../vpn.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = [{
+    name = "root";
+    device = "/dev/nvme0n1p2";
+    preLVM = true;
+    allowDiscards = true;
+  }];
+
+  networking.hostName = "x1cg6";
+}
