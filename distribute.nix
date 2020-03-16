04301c27900d6f17c35a1885a5767f8fef6b5579
Benjamin Hipple <bhipple@protonmail.com>
2020-03-15 21:31:20 -0400

    Working custom AMI and first attempt at spot market

--- distribute.nix
+++ distribute.nix
@@ -1,5 +1,5 @@
 # Off in its own file so I can toggle/import it easily across contexts.
 {
-  #distributedBuilds = false;
-  distributedBuilds = true;
+  distributedBuilds = false;
+  # distributedBuilds = true;
 }
