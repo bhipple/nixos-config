21f656b6709af6333259dd86d5e059feac549072
Benjamin Hipple <bhipple@protonmail.com>
2021-08-07 16:23:44 -0400

    update nixos hardware

--- brh.x1cg6/configuration.nix
+++ brh.x1cg6/configuration.nix
@@ -1,17 +1,9 @@
 { config, pkgs, ... }:
 let
   nixos-hardware = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixos-hardware/archive/adecd1113c2d4137ef23237f9af450736fd8d2cc.tar.gz";
-    sha256 = "1k4zl5c0ak525japzxmddimzac1szhlwd83b7i4v24a32lbwhlkz";
+    url = "https://github.com/NixOS/nixos-hardware/archive/8296b88560d8ac07a885452e094cd454de90ea9b.tar.gz";
+    sha256 = "07s1p1qj5knh71lq3nzkxs3mhh5n9dbf6qi87dhkkngp85fjv9il";
   };
-
-  nixpkgs-unstable = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a36056d068770432a5203ba86e1a3accdc71aa76.tar.gz";
-    sha256 = "0j24chzb6xm1hpizd0yw24iiwnab2sy80dsxz26am6xhc0nrm9j1";
-  };
-
-  unstable-pkgs = import nixpkgs-unstable { };
-
 in
 {
   imports = [
@@ -47,7 +39,6 @@ in
   services = {
     fwupd = {
       enable = true;
-      #package = unstable-pkgs.fwupd;
     };
 
     jenkins = {
