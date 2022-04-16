ddc02dd8912b02991cd42f748be5b3c74a6eaf93
Benjamin Hipple <bhipple@protonmail.com>
2022-04-16 11:58:28 -0400

    organize

--- sudo.nix
+++ sudo.nix
@@ -1,21 +1,23 @@
 { pkgs, ... }:
 {
   security.sudo.extraConfig = ''
+    # Nix
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/nix-channel --update
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/nix-store --optimize
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/nixos-rebuild
 
+    # Misc cmds
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/bluetoothctl
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/poweroff
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/reboot
+    bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/slock
 
+    # Systemctl
+    bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/systemctl restart bluetooth
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/systemctl restart display-manager
-
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/systemctl restart openvpn-protonvpn
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/systemctl start openvpn-protonvpn
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/systemctl status openvpn-protonvpn
     bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/systemctl stop openvpn-protonvpn
-
-    bhipple ALL = (root) NOPASSWD: /run/current-system/sw/bin/slock
   '';
 }
