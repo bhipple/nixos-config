6972e50c7b9efcf10a9fe5281ae21cc4340177a1
Benjamin Hipple <bhipple@protonmail.com>
2025-10-05 21:35:08 +0000

    up

--- brh.wsl/configuration.nix
+++ brh.wsl/configuration.nix
@@ -1,12 +1,6 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
 { config, lib, pkgs, ... }:
-
 {
   imports = [
     # include NixOS-WSL modules
@@ -23,11 +17,12 @@
     pinentry-curses
     python3
     slock
-    tmux
     vim
     wget
     xautolock
     xorg.xmessage
+    zellij
+    zsh
   ];
 
   nix.settings.experimental-features = "nix-command flakes";
