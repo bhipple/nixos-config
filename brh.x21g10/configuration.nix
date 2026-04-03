0eecf4e9327d3b3567f57650ecc6722a87fdd753
Benjamin Hipple <bhipple@protonmail.com>
2026-04-03 00:24:11 -0400

    gnome

--- brh.x21g10/configuration.nix
+++ brh.x21g10/configuration.nix
@@ -68,10 +68,6 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
